22c12d5d4c4a34f448a13297f428da28e59e417c
Igor Sheverda <ktgtoit@gmail.com>
2023-06-14 16:10:52 +0300

    Initial Commit

---
+++ NewsApp_/NewsApp_App.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp_App.swift
+//  NewsApp_
+//
+//  Created by user on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
